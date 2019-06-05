a24d90c03eabf17d843b63fbb1547a556399a4df
Manoel <manoelfernandes15@gmail.com>
2019-06-05 18:33:01 -0300

    primeiro commit projeto criado

---
+++ src/main/java/com/manoelneto/planodenegocios/BancodedadosPlanodenegociosApplication.java
@@ -0,0 +1,13 @@
+package com.manoelneto.planodenegocios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancodedadosPlanodenegociosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancodedadosPlanodenegociosApplication.class, args);
+	}
+
+}
